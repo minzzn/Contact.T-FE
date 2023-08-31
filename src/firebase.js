761d67b8d981bab5d8bfd8d883c8ffefe9636179
krsy0411 <krsy0411@naver.com>
2023-08-31 19:08:00 +0900

    apiKey 추적 못하게끔 변경

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { googleApiKey } from "./apiKey";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC6NBWla3-LwR5X2UNxW9gyQMGSQR5ioGE",
+  apiKey: googleApiKey,
   authDomain: "deceit-cat-44184.firebaseapp.com",
   projectId: "deceit-cat-44184",
   storageBucket: "deceit-cat-44184.appspot.com",

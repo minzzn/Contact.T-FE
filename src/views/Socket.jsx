0eab3f80b58754abf7105b27a7b5e4a52ec8e7eb
krsy0411 <krsy0411@naver.com>
2023-09-06 19:19:19 +0900

    리팩토링 하다가 에러 발견 후 정지

--- src/views/Socket.jsx
+++ src/views/Socket.jsx
@@ -1,13 +1,29 @@
+import { useEffect } from "react";
+// <script src="/socket.io/socket.io.js"></script>
+// <script src="/chat.js"></script>
+
 export const Socket = () => {
   // todo : script 태그 동적으로 집어넣기
-  const scripts = `
-  <script src="/socket.io/socket.io.js"></script>
-  <script src="/chat.js"></script>
-  `;
+
+  // 만약 첫 마운팅이 이뤄졌다면
+  useEffect(() => {
+    // const rootTag = document.querySelector("#root");
+    for (let i = 0; i < 2; i++) {
+      const script = document.createElement("script");
+      if (i === 0) {
+        script.src = "/socket.io/socket.io.js";
+      } else {
+        script.src = "/src/chat.js";
+        script.defer = true;
+      }
+      document.body.appendChild(script);
+    }
+  }, []);
 
   return (
     <>
-      <div class="wrapper">
+      <h1>Hello!</h1>
+      {/* <div class="wrapper">
         <div class="user-container">
           <label for="nickname">유저명</label>
           <input type="text" id="nickname" />
@@ -21,7 +37,7 @@ export const Socket = () => {
             <button class="send-button">전송</button>
           </span>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };

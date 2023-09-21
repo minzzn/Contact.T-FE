768a34149c93768c2ba967cdc8893cdf1044d698
krsy0411 <krsy0411@naver.com>
2023-09-21 16:49:37 +0900

    1차 파일 청소

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import "./css/style.css";
 import { Route, Routes } from "react-router-dom";
 import { Register } from "./views/Register";
 import { Login } from "./views/Login";
-import { Socket } from "./views/Socket";
-
+import { Main } from "./views/Main";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Register />} />
+      <Route path="/" element={<Main/>}/>
+      <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/socket" element={<Socket />} />
     </Routes>
   );
 }

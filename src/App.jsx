fbca5ff557e55c5a5b88548ffccbf036c17d416a
PANdas <krsy0411@naver.com>
2023-10-12 17:48:29 +0900

    컨벤션 맞추기

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import { Route, Routes } from "react-router-dom";
 import { Register } from "./views/Register";
 import { Login } from "./views/Login";
 import { Main } from "./views/Main";
+import { SetProfile } from "./components/SetProfile";
+import { ViewProfile } from "./components/ViewProfile";
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Main/>}/>
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/Setprofile" element={<SetProfile />} />
+      <Route path="/Viewprofile" element={<ViewProfile />} />
     </Routes>
   );
 }

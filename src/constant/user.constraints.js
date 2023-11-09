1338c2d480f2738a0695731687adae68500fe773
PANdas <krsy0411@naver.com>
2023-11-10 00:38:01 +0900

    정규표현식 에러 수정

--- src/constant/user.constraints.js
+++ src/constant/user.constraints.js
@@ -1,18 +1,18 @@
 export const MinimumLength = (limit) => ({
-    rule: new RegExp(`(.)(${limit})`),
+    rule: new RegExp(`^.{${limit},}$`),
     message: `최소 ${limit}글자 이상입니다`
 });
 
 // . : 모든 문자열(숫자, 한글, 영어, 특수기호, 공백 모두)
 // + : 최소 한개 or 여러개
 export const isRequired = {
-    rule: /\.+/,
+    rule: /.+/,
     message: '필수 입력 항목입니다',
 };
 // \s : space 공백
-export const CantStartWithSpace = {
-    rule: /\s/,
-    message: '공백으로 시작할 수 없습니다',
+export const CantContainSpace = {
+    rule: /^[^\s]+$/,
+    message: '공백을 포함할 수 없습니다',
 };
 // ^문자열 : 특정 문자열로 시작 (시작점)
 // \d : 숫자
@@ -27,6 +27,6 @@ export const EmailFormat = {
 }
 
 export const SpecialText = {
-    rule: /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g,
+    rule: /[!?@#$%^&*():;+-=~{}<>\_\[\]\|\\\"\'\,\.\/\`\₩]/g,
     message: '특수문자를 포함해주셔야 합니다',
 }

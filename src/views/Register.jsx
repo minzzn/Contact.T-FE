50322404bf43463265fe0c0e688e94794bf1807d
PANdas <krsy0411@naver.com>
2023-11-10 00:38:32 +0900

    에러 디버깅 완료

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { ErrorMsgContainer, FormInnerWrapper, LoginInput, LoginSubmitButton, LoginTitle, StyledForm, StyledLabel, StyledLink } from "../css/styled/signin_up.styled";
 import { useNavigate } from "react-router-dom";
-import { isRequired, MinimumLength, CantStartWithNumber, CantStartWithSpace, EmailFormat, SpecialText } from "../constant/user.constraints";
+import { isRequired, MinimumLength, CantStartWithNumber, CantContainSpace, EmailFormat, SpecialText } from "../constant/user.constraints";
 
 export const Register = () => {
     const navigate = useNavigate();
@@ -13,17 +13,13 @@ export const Register = () => {
     const [error, setError] = useState("");
 
     function validation(value, constraints) {
-        console.log('검증 작업 함수 진입');
         const result = [];
-        
         // 만약 제한 조건으로 들어온 값들 중에 false로 넘어온 값들이 있으면 그걸 배열에 다시 담습니다.
         constraints.map((constraint) => {
             if(constraint.rule.test(value) === false) {
                 result.push(constraint.message);
             }
         })
-        
-        console.log(result);
         
         // 제한 조건에 해당 안 되는게 하나라도 있으면 배열 중 가장 앞 값을 반환
         return (result.length > 0) ? result[0] : null;
@@ -39,23 +35,23 @@ export const Register = () => {
         switch(name) {
             case "name":
                 setName(value);
-                result = validation(value, [isRequired, MinimumLength(2), CantStartWithSpace, CantStartWithNumber]);
+                result = validation(value, [isRequired, MinimumLength(2), CantContainSpace]);
                 if(result === null) {
-                    validValues[0] = true;
+                    validRules[0] = true;
                 }
                 break;
             case "email":
                 setEmail(value);
-                result = validation(value, [isRequired, CantStartWithSpace, CantStartWithNumber, EmailFormat]);
+                result = validation(value, [isRequired, CantContainSpace, EmailFormat]);
                 if(result === null) {
-                    validValues[1] = true;
+                    validRules[1] = true;
                 }
                 break;
             case "password":
                 setPassword(value);
-                result = validation(value, [isRequired, MinimumLength(8), SpecialText]);
+                result = validation(value, [isRequired, CantContainSpace, MinimumLength(8), SpecialText]);
                 if(result === null) {
-                    validValues[2] = true;
+                    validRules[2] = true;
                 }
                 break;
         }

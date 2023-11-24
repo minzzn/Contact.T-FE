16302ae70863120f4f026fc13585a92bea030d66
PANdas <krsy0411@naver.com>
2023-11-24 15:05:59 +0900

    서버와 통신에 필요한 함수들 생성

---
+++ src/function/common.js
@@ -0,0 +1,32 @@
+import { serverEndPoint } from "../constant/common.constant";
+
+export const getToken = () => localStorage.getItem("token");
+export const setToken = (tokenValue) => localStorage.setItem("token", tokenValue);
+
+// 토큰과 string으로 뒤에 추가적으로 붙는 url을 적어주세요
+export const getUserInfoThrough = async (token) => {
+    const username = "";
+    const BACK_API = `${serverEndPoint}main`;
+
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                // 인증을 위한 request임을 짐작해주세요
+                'Authorization': `Bearer ${token}`,
+            },
+        })
+
+        // 전송은 됐지만 응답이 제대로 안 넘어온 경우
+        if(!response.ok) {
+            const errorData = response.json();
+            throw new Error(errorData.message || '유저 정보를 가져오는 데 실패함');
+        }
+        // 유저 정보를 반환
+        return response.json();
+
+    } catch(error) {
+        // 전송에 실패한 경우의 추가 로직을 써도 됩니다.
+        console.error('전송 실패:', error.message);
+    }
+};
